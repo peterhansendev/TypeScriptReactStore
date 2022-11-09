d3e7abf60cbdf03200ef3a230e358605a2fcf354
peterhansendev <114914348+peterhansendev@users.noreply.github.com>
2022-11-09 15:33:21 +0100

    ok

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,11 @@ export function Navbar() {
   return (
     <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Store
+          </Nav.Link>
+        </Nav>
         {cartQuantity > 0 && (
           <Button
             onClick={openCart}
